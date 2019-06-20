c7a7f7991b74a030319ae973e09b59ec7ae18735
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2019-06-20 23:13:44 +0200

    create reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,15 @@
 import { combineReducers } from 'redux';
 
+import team from './team';
+import master from './master';
+import turn from './turn';
+import words from './words';
+import colors from './colors';
+
 export default combineReducers({
+  team,
+  master,
+  turn,
+  words,
+  colors,
 });
---
+++ src/reducers/master.js
@@ -0,0 +1,7 @@
+const initialState = false;
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
