6b94c527b10ba6d9cfac0061b00019ac5399b52f
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2020-03-16 20:09:05 +0100

    game over message

--- src/reducers/turn.js
+++ src/reducers/turn.js
@@ -5,6 +5,9 @@ export default (state = initialState, action) => {
     case 'REVEAL':
     case 'PASS':
       return action.turn;
+    case 'OVER':
+      alert(`Team ${['red', 'blue'][action.winner]} wins!`);
+      return -1;
     default:
       return state;
   }
